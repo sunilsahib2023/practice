4472b0bacd4369e6752d80a08afc4a43003ab217
sunilsahib2023 <116146133+sunilsahib2023@users.noreply.github.com>
2024-08-12 01:05:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,3 +2,72 @@
 <?Php
 echo "<h1>Welcome to PHP </h!>";
 ?>
+<HTML>
+
+<BODY>
+
+
+<?PHP
+include("header.php");
+echo "<h1>Funtion in php </h1>";
+
+// function with parameter and return value
+
+
+function sum($a,$b)
+{
+return($a+$b);
+}
+
+echo "Sum of 23 and 45 is = ".sum(23,45)."<br>";
+
+function draw($char,$length,$co)
+{
+
+   for($i=0;$i<=$length;$i++)
+     {
+       echo "<span style='color:".$co."'>&".$char.";</span>";
+
+      }
+
+echo "<br>";
+
+}
+
+draw('hearts',5,"green");
+draw('clubs',10,"red");
+draw('diams',15,"blue");
+draw('spades',20,"orange");
+
+print "Creating bar chart<br><BR>";
+
+function barchart($length,$co,$w,$h)
+{
+
+    for($i=0;$i<=$length;$i++)
+      {  
+      
+           echo "<div  style='float:left;width:".$w."px;height:".$h."px;;display:inline;background-color:".$co."'><span style='float:left;width:5px;'></span></div>" ; 
+   
+      }
+
+
+
+}
+
+
+for($i=0;$i<=6;$i++)
+{
+
+
+barchart(10,rand(40,255),2,rand(40,150));
+
+
+
+}
+
+?>
+
+<body>
+
+</html>
